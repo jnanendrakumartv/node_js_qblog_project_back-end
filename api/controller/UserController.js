bfd4127ea765d7a9dcce22208051409a34e259b8
jnanendra <vindya.sk1991@gmail.com>
2019-08-08 15:47:56 +0530

    signup tocken generate

--- api/controller/UserController.js
+++ api/controller/UserController.js
@@ -80,6 +80,46 @@ exports.userSignup = function(req, res){
 //   });
 // };
 
+// exports.userSignin = (req,res,next) =>{
+//   const email = req.body.email;
+//   const password = req.body.password;
+//   let loadedUser;
+//   UserData.findOne({email: email})
+//   .then(user =>{
+//     if(!user){
+//       const error = new Error('A user with this email could not be found.');
+//       error.statusCode = 401;
+//       throw error;
+//     }
+//     loadedUser = user;
+//     return bcrypt.compare(password,user.password);
+//   })
+//   .then(isEqual =>{
+//     if(!isEqual){
+//       const error = new Error('wrong password.');
+//       error.statusCode = 401;
+//       throw error;
+//     }
+//     const token = jwt.sign(
+//     {
+//       email: loadedUser.email,
+//       userId:loadedUser._id.toString()
+//     },'secret',)
+//     res.status(200).json({token: token, userId: loadedUser._id.toString()})
+//     res.json({
+//       success: true,
+//       token: token
+//   });
+//   })
+//   .catch(err => {
+//     if (!err.statusCode) {
+//       err.statusCode = 500;
+//     }
+//     next(err);
+//   }); 
+// }
+
+
 exports.userSignin = (req,res,next) =>{
   const email = req.body.email;
   const password = req.body.password;
@@ -92,25 +132,26 @@ exports.userSignin = (req,res,next) =>{
       throw error;
     }
     loadedUser = user;
-    return bcrypt.compare(password,user.password);
-  })
-  .then(isEqual =>{
-    if(!isEqual){
-      const error = new Error('wrong password.');
-      error.statusCode = 401;
-      throw error;
-    }
     const token = jwt.sign(
-    {
-      email: loadedUser.email,
-      userId:loadedUser._id.toString()
-    },'secret',)
-    res.status(200).json({token: token, userId: loadedUser._id.toString()})
-    res.json({
-      success: true,
-      token: token
-  });
+      {
+        email: loadedUser.email,
+        userId:loadedUser._id.toString()
+      },'secret')
+      return res.status(200).json({token: token, userId: loadedUser._id.toString(), email: loadedUser.email})
+      // res.json({
+        // success: true,
+        // token: token
+    // });
+    // })
+    // return bcrypt.compare(password,user.password);
   })
+  // .then(isEqual =>{
+    // if(!isEqual){
+    //   const error = new Error('wrong password.');
+    //   error.statusCode = 401;
+    //   throw error;
+    // }
+
   .catch(err => {
     if (!err.statusCode) {
       err.statusCode = 500;
@@ -118,6 +159,11 @@ exports.userSignin = (req,res,next) =>{
     next(err);
   }); 
 }
+
+
+
+
+
 
 exports.getAllSignin = (isAuth,function(req, res) {
   UserData.find({userId:req.decodedToken}, function(err, data) {
